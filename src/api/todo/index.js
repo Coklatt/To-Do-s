45de78787896a4ccd561b5fcad17cab4a4f09151
reader00 <qoryamanahputra0708@gmail.com>
2023-12-07 16:42:29 +0700

    push no-deploy

--- src/api/todo/index.js
+++ src/api/todo/index.js
@@ -3,7 +3,7 @@ const routes = require('./routes');
 
 module.exports = {
     name: 'todo',
-    version: '1.1.0',
+    version: '1.0.0',
     register: async (server, { service, tasksService, validator }) => {
         const toDosHandler = new ToDosHandler(service, tasksService, validator);
         server.route(routes(toDosHandler));
